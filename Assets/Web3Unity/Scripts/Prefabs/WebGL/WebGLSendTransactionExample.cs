240f28d4230e12181940afdc6ef438b66680665b
ninonesix <mninonesix@gmail.com>
2022-06-24 23:12:11 +0700

    + Add game scene

--- Assets/Web3Unity/Scripts/Prefabs/WebGL/WebGLSendTransactionExample.cs
+++ Assets/Web3Unity/Scripts/Prefabs/WebGL/WebGLSendTransactionExample.cs
@@ -2,16 +2,18 @@ using System;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.SceneManagement;
+using TMPro;
 
 #if UNITY_WEBGL
 public class WebGLSendTransactionExample : MonoBehaviour
 {
-    async public void OnSendTransaction()
+    public async void OnSendTransaction()
     {
         // account to send to
-        string to = "0x428066dd8A212104Bc9240dCe3cdeA3D3A0f7979";
+        string to = "0x814220bA2E552Db82615980966220C5c3f67d3b0";
         // amount in wei to send
-        string value = "12300000000000000";
+        string value = "5000000000000000000";
         // gas limit OPTIONAL
         string gasLimit = "";
         // gas price OPTIONAL
@@ -20,8 +22,19 @@ public class WebGLSendTransactionExample : MonoBehaviour
         try {
             string response = await Web3GL.SendTransaction(to, value, gasLimit, gasPrice);
             Debug.Log(response);
+            hash.text = response;
         } catch (Exception e) {
             Debug.LogException(e, this);
+        }
+    }
+    
+    public TextMeshProUGUI hash;
+
+    public void Update()
+    {
+        if (hash.text.Contains("0"))
+        {
+            SceneManager.LoadScene(2);
         }
     }
 }
