2c2bb0fb2951ab79cc6a494875b9526ee851ba93
ninonesix <mninonesix@gmail.com>
2022-07-02 22:05:16 +0700

    + Update login screen

---
+++ Assets/Scripts/AnimButton.cs
@@ -0,0 +1,61 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using DG.Tweening;
+using UnityEngine;
+using UnityEngine.UI;
+using DG.Tweening;
+
+public class AnimButton : MonoBehaviour
+{
+    [Header("Attributes")]
+    [SerializeField] private bool bounced;
+
+    [SerializeField] private bool dropped;
+
+    [SerializeField] private bool scaledOverTime;
+
+    [Header("Timing")]
+    //[SerializeField] private float fadeOutBounce = 0.1f;
+    //[SerializeField] private float fadeInBounce = 0.1f;
+
+    private Sequence seq;
+    private Button btn;
+    private RectTransform rect;
+    private IEnumerator scaleOverTime;
+
+    private void Awake()
+    {
+        btn = GetComponent<Button>();
+        rect = GetComponent<RectTransform>();
+        btn.onClick.AddListener(OnClickedAction);
+
+        if(scaledOverTime)
+        {
+            var seq = DOTween.Sequence();
+            seq.Append(transform.DOScale(1.5f, 0.5f));
+            seq.Append(transform.DOScale(1f, 0.5f));
+            seq.SetLoops(-1);
+        }
+    }
+
+    private void OnClickedAction()
+    {
+        seq.Kill();
+        seq = DOTween.Sequence();
+
+        if (bounced)
+        {
+            seq.Append(transform.DOScale(0.9f, 0.1f));
+            seq.Append(transform.DOScale(1f, 0.1f));
+        }
+
+        if (dropped)
+        {
+            var pos = rect.anchoredPosition;
+            var start = pos + Vector2.down * 30f;
+            seq.Insert(0, rect.DOAnchorPos(start, 0.15f));
+            seq.Insert(0.15f, rect.DOAnchorPos(pos, 0.15f));
+        }
+    }
+}
--- Assets/Web3Unity/Scripts/Scenes/WebLogin.cs
+++ Assets/Web3Unity/Scripts/Scenes/WebLogin.cs
@@ -3,7 +3,7 @@ using System.Runtime.InteropServices;
 using UnityEngine;
 using UnityEngine.SceneManagement;
 
-#if UNITY_WEBGL
+//#if UNITY_WEBGL
 public class WebLogin : MonoBehaviour
 {
     [DllImport("__Internal")]
@@ -47,4 +47,4 @@ public class WebLogin : MonoBehaviour
         SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex + 1);
     }
 }
-#endif
+//#endif
